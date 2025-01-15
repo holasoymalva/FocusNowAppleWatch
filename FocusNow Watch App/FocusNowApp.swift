97e19cb2b568f2ca0daab0b382e3b1b65a7f95ba
holasoymalva <alquimista3301@gmail.com>
2025-01-14 21:39:38 -0600

    Initial Commit

---
+++ FocusNow Watch App/FocusNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusNowApp.swift
+//  FocusNow Watch App
+//
+//  Created by malva on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FocusNow_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FocusNow Watch AppTests/FocusNow_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FocusNow_Watch_AppTests.swift
+//  FocusNow Watch AppTests
+//
+//  Created by malva on 14/01/25.
+//
+
+import Testing
+@testable import FocusNow_Watch_App
+
+struct FocusNow_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
